c29ce55b4a6d7dba8dca06b7039ab83dfb5fea15
doc97 <daniel.riissanen@gmail.com>
2019-01-15 11:44:12 +0200

    osa1: Add code for 1.1 and 1.2

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
+const Header = (props) => (
   <div>
-    <p>Hello world</p>
+    <h1>{props.course}</h1>
   </div>
 )
+
+const Part = (props) => (
+  <>
+    <p>{props.name} {props.exercise}</p>
+  </>
+)
+
+const Content = (props) => (
+  <div>
+    <Part name={props.names[0]} exercise={props.exercises[0]} />
+    <Part name={props.names[1]} exercise={props.exercises[1]} />
+    <Part name={props.names[2]} exercise={props.exercises[2]} />
+  </div>
+)
+
+const Total = (props) => {
+  let exerciseSum = 0;
+  for (let e of props.exercises) {
+    exerciseSum += e;
+  }
+  return (
+    <div>
+      <p>yhteensä {exerciseSum}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys';
+  const exercises = [ 10, 7, 14 ];
+  const names = [
+    'Reactin perusteet',
+    'Tiedonvälitys propseilla',
+    'Komponenttien tila'
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content names={names} exercises={exercises}/>
+      <Total exercises={exercises}/>
+    </div>
+  )
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
