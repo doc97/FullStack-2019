eac1f1a8b82432e56875f41cc3c1cc6f9a8ae93c
doc97 <daniel.riissanen@gmail.com>
2019-02-18 23:26:10 +0200

    osa4: 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.map(b => b.likes).reduce((acc, cur) => acc + cur)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
